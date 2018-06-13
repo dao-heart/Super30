d1deebcf4ace61edd242d716c5ea1a4a18f94faf
Shruti Sheoran <shruti.sheoran03@gmail.com>
2018-06-13 16:15:57 +0530

    Overloaded inbuilt print function

--- Assignment6/MethodOverloading.java
+++ Assignment6/MethodOverloading.java
@@ -1,18 +1,10 @@
 public class MethodOverloading {
-    public void print() {
-        System.out.println("Function with null arguments called");
-    }
-    public void print(int arg) {
-        System.out.println("Function with int type argument called");
-    }
-    public void print(float arg) {
-        System.out.println("Function with float type argument called");
-    }
 
     public static void main(String[] args) {
-        MethodOverloading mo = new MethodOverloading();
-        mo.print();
-        mo.print(1);
-        mo.print(4.5f);
+
+        //Overloading of inbuilt print function
+        System.out.print((Integer)null);
+        System.out.println();
+        System.out.print((Float)null);
     }
 }
